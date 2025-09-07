84b54fd36c4f6df9e42c259e32f80b661492b126
Estêvão Santos <estevaojs@gmail.com>
2025-09-07 19:35:54 -0300

    ref(timeseries): removed unused code.

--- pipeline_5g/utils/timeseries.py
+++ pipeline_5g/utils/timeseries.py
@@ -1,11 +1,7 @@
-import numpy as np
 import pandas as pd
 from darts import TimeSeries
-from darts.utils.missing_values import (
-    extract_subseries,
-    fill_missing_values,
-    missing_values_ratio,
-)
+from darts.utils.missing_values import fill_missing_values
+
 
 def create_target_timeseries(data: pd.DataFrame, targets: list[str], timestamp_col: str ="Timestamp", freq: str | None ="s") -> TimeSeries | None:
     """
@@ -77,44 +73,4 @@ def impute_timeseries_missing_values(
     # see pandas.DataFrame.interpolate for params
     ts_imputed_initial = fill_missing_values(ts)
 
-    # if ts_imputed_initial.width > 1:
-    #     # Multivariada
-    #     components_data = []
-    #     for component_name in ts_imputed_initial.components:
-    #         comp_ts = ts_imputed_initial[component_name]
-    #         values = comp_ts.values(copy=False)
-
-    #         if np.isnan(values).all():
-    #             if fill_all_nan_with is not None:
-    #                 filled = pd.Series(fill_all_nan_with, index=comp_ts.time_index, name=component_name)
-    #             else:
-    #                 filled = pd.Series(np.nan, index=comp_ts.time_index, name=component_name)
-    #         else:
-    #             filled = comp_ts.to_series().bfill()
-    #             if filled.isnull().any() and fill_all_nan_with is not None:
-    #                 filled = filled.fillna(fill_all_nan_with)
-
-    #         components_data.append(filled)
-
-    #     df_final = pd.concat(components_data, axis=1)
-    #     ts_imputed = TimeSeries.from_dataframe(df_final, freq=ts_imputed_initial.freq_str, fill_missing_dates=True)
-
-    # else:
-    #     # Univariada
-    #     values = ts_imputed_initial.values(copy=False)
-    #     comp_name = ts_imputed_initial.components[0]
-
-    #     if np.isnan(values).all():
-    #         if fill_all_nan_with is not None:
-    #             filled = pd.Series(fill_all_nan_with, index=ts_imputed_initial.time_index, name=comp_name)
-    #             ts_imputed = TimeSeries.from_series(filled, freq=ts_imputed_initial.freq_str, fill_missing_dates=True)
-    #         else:
-    #             ts_imputed = ts_imputed_initial
-    #     else:
-    #         filled = ts_imputed_initial.to_series().bfill()
-    #         if filled.isnull().any() and fill_all_nan_with is not None:
-    #             filled = filled.fillna(fill_all_nan_with)
-    #         ts_imputed = TimeSeries.from_series(filled, freq=ts_imputed_initial.freq_str, fill_missing_dates=True)
-
-    # return ts_imputed
     return ts_imputed_initial
